8a8b01d5d3d14ef8718a5bac6f50a780d64c2038
zewenn <takaczsoli@gmail.com>
2024-08-12 14:44:49 +0200

    events added

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,31 @@
 const std = @import("std");
 const print = std.debug.print;
-const String = @import("./deps/zig-string.zig").String;
+
 const input = @import("./deps/zig-input.zig");
+const String = @import("./deps/zig-string.zig").String;
+
+const events = @import("./sys/events.zig");
+
+pub fn testfn() void {
+    print("x", .{});
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    // var allocator = gpa.allocator();
+    var allocator = gpa.allocator();
+
+    events.init(&allocator);
+    defer events.deinit();
+
+    try events.on("update", testfn);
 
     var last_char: u8 = 0;
 
     while (true) {
-        print("\x1b[H\n", .{});
+        print("\x1b[2J\x1b[H", .{});
+        // print("\x1b[H\n", .{});
         print("[DEBUG] Last char: {?}\n\n", .{last_char});
 
         const input_char = input.getKeyDown();
@@ -22,5 +35,6 @@ pub fn main() !void {
             input.keys.ESCAPE => break,
             else => {},
         }
+        try events.call("update");
     }
 }
---
+++ src/sys/events.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+
+const map_fn_type = *const fn () void;
+const map_fn_struct_type = struct { func: map_fn_type };
+const map_type = std.StringHashMap(std.ArrayListAligned(map_fn_struct_type, null));
+
+var event_map: map_type = undefined;
+var allocator_ptr: *std.mem.Allocator = undefined;
+
+pub fn init(allocator: *std.mem.Allocator) void {
+    event_map = map_type.init(allocator.*);
+    allocator_ptr = allocator;
+}
+
+pub fn deinit() void {
+    var iterator = event_map.keyIterator();
+
+    while (iterator.next()) |item| {
+        const arr = event_map.get(item.*);
+        if (arr != null) arr.?.deinit();
+
+        _ = event_map.remove(item.*);
+    }
+    event_map.deinit();
+}
+
+pub fn on(comptime id: []const u8, comptime func: map_fn_type) !void {
+    // if (event_map == undefined) {
+    //     return error.UndefinedError;
+    // }
+    // try event_map.put(id, func);
+    const data = event_map.get(id);
+
+    if (data == null) {
+        var new_array = std.ArrayList(map_fn_struct_type).init(allocator_ptr.*);
+        try new_array.append(map_fn_struct_type{ .func = func });
+        try event_map.put(id, new_array);
+        return;
+    }
+
+    try @constCast(&data.?).append(map_fn_struct_type{ .func = func });
+    return;
+}
+
+pub fn call(comptime id: []const u8) !void {
+    // if (event_map == undefined) {
+    //     return error.UndefinedError;
+    // }
+
+    const data = event_map.get(id);
+
+    if (data == null) {
+        return;
+    }
+
+    for (data.?.items) |func_struct| {
+        func_struct.func();
+    }
+}
+
+pub fn delete(comptime id: []const u8) !void {
+    _ = event_map.remove(id);
+}
